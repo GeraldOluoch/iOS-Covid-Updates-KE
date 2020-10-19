d96a73af354da94948ca4a899870178e7ca82e4a
Gerald Oluoch <94605284s@gmail.com>
2020-10-19 19:37:50 +0300

    Initial Commit

---
+++ Covid Updates -KE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covid Updates -KE
+//
+//  Created by Gerald on 19/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
