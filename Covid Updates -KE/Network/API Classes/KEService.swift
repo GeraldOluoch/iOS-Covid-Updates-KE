f96849f91994d2f3701ebcbd7012ccd17cedfd0d
Gerald <94605284s@gmail.com>
2020-10-22 16:21:43 +0300

    KEService  API added

---
+++ Covid Updates -KE/Network/API Classes/KEService.swift
@@ -0,0 +1,60 @@
+//
+//  KEService.swift
+//  Covid Updates -KE
+//
+//  Created by Gerald on 22/10/2020.
+//
+
+import Foundation
+import Moya
+
+public enum KEService {
+    case Request(name: String)
+}
+
+extension KEService: TargetType {
+    
+    public var baseURL: URL {return URL(string: "https://rapidapi.p.rapidapi.com")! }
+    
+    public var path: String {
+        switch self {
+        case .Request:
+            return "/country"
+        }
+    }
+    
+    public var method: Moya.Method {
+        switch self {
+        case .Request:
+        return .get
+        }
+    }
+    
+    public var task: Task {
+        switch self {
+        case .Request(let name):
+        return .requestParameters(parameters: ["name": name], encoding: URLEncoding.default)
+        }
+    }
+    
+    public var sampleData: Data {
+        switch self {
+        case .Request:
+        return ("{}".data(using: String.Encoding.utf8)! as NSData) as Data
+        }
+    }
+    
+    public var mehod: Moya.Method {
+        switch self {
+        case .Request:
+        return .get
+        }
+    }
+    
+    public var headers: [String: String]? {
+        return ["x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+                "x-rapidapi-key" : "3bee4b7e02msh0cf90a5b2a0ca73p15204cjsn43ca8fe36992"]
+    }
+}
+    
+
