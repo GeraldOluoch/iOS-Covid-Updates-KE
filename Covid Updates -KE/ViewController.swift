1ae9e207a489754e205ad181187cec4a5def70ec
Gerald Oluoch <94605284s@gmail.com>
2020-11-19 23:39:41 +0300

    Added TODO list entry

--- Covid Updates -KE/ViewController.swift
+++ Covid Updates -KE/ViewController.swift
@@ -72,5 +72,6 @@ extension ViewController {
 //TODO: Print complete JSON upon loading view
 //TODO: Pass data to UILabel in view
 //TODO: Fix Flag UI
+//TODO: Change JSON call from using Moya to default iOS call
 
 
