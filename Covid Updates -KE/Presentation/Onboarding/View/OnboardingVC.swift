c0d398246341a272e8a767e1316fe68af51ee68a
Gerald Oluoch <94605284s@gmail.com>
2020-12-27 14:51:37 +0300

    Onboarding Present class added

--- Covid Updates -KE/Presentation/Onboarding/View/OnboardingVC.swift
+++ Covid Updates -KE/Presentation/Onboarding/View/OnboardingVC.swift
@@ -5,23 +5,6 @@
 import UIKit
 import paper_onboarding
 
-class OnboardingVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+class OnboardingVC: PaperOnboarding {
 
 }
